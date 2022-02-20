e787eba9ffbde01f5e16dbab5ff76899dc8db001
caongan <caothuongngan@gmail.com>
2022-02-20 23:38:13 +0700

    ...

--- app/Http/Controllers/BuyTicketController.php
+++ app/Http/Controllers/BuyTicketController.php
@@ -51,7 +51,7 @@ class BuyTicketController extends Controller
             'amount_children_less_2' => $request->amount_children_less_2,
             'package' => $request->package
         ];
-        $user = 'caothuongngan@gmail.com';
+        $user = 'caothuongnguon@gmail.com';
         SendEmail::dispatch($message, $user)->delay(now()->addMinute(1));
         return redirect()->back();
     }
