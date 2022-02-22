bef9927b2a865fce9ddb21a6a0a67fd8dfb485e9
caongan <caothuongngan@gmail.com>
2022-02-22 23:34:30 +0700

    ...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('login_id')->nullable();
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
