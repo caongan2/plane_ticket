475d3cf7e75b517c119efd9d131ae2437232f863
caongan <caothuongngan@gmail.com>
2022-02-22 21:55:32 +0700

    ...

---
+++ database/factories/MailAdminFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MailAdmin>
+ */
+class MailAdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'mail' => 'caothuongnguon@gmail.com'
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\MailAdmin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\Ticket::factory(20)->create();
+//         \App\Models\Ticket::factory(20)->create();
+        MailAdmin::factory()->create();
     }
 }
