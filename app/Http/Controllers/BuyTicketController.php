897fa7708bd54ae368ad6b10e81fdb117231d9b8
caongan <caothuongngan@gmail.com>
2022-02-24 21:21:01 +0700

    ...

--- app/Http/Controllers/BuyTicketController.php
+++ app/Http/Controllers/BuyTicketController.php
@@ -98,8 +98,13 @@ class BuyTicketController extends Controller
     {
         $query1 = DB::table('ticket')->where('status', 0)->orderByDesc('id')->get();
         $query2 = DB::table('ticket')->where('status', 1)->orderByDesc('id')->get();
-        $revenue = DB::table('ticket')->where('status', 1)->get()->count();
-        $revenue_by_user = DB::table('ticket')->where('status', 1)->where('user_update', auth()->id())->get()->count();
+        $revenue = DB::table('ticket')
+            ->where('status', 1)
+            ->sum('price');
+        $revenue_by_user = DB::table('ticket')
+            ->where('status', 1)
+            ->where('user_update', auth()->id())
+            ->sum('price');
          return view('get-all', compact('query1', 'query2', 'revenue', 'revenue_by_user'));
     }
 
